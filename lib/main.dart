14a5100decd68261bca2c5544f7bd9ca58cd5054
Sachin <sachinsaini854@gmail.com>
2021-03-05 00:59:20 +0530

    1st Commit to Timer App

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TimerState();
+  }
+}
+
+class TimerState extends State<MyApp> {
+  var increasetime = 0;
+  void decreasTime() {
+    setState(() {
+      increasetime = 0;
+    });
+  }
+
+  void increasTime() {
+    setState(() {
+      increasetime = increasetime + 1;
+      print(increasetime);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text("Long Press Test",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ))),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              RaisedButton(
+                child: Text("Increment"),
+                onPressed: increasTime,
+                onLongPress: decreasTime,
+              ),
+              Text("$increasetime")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
