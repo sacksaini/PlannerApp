47b01c8d353393aa6e4af1e0c67abf0b09b8a306
Sachin <sachinsaini854@gmail.com>
2021-03-14 02:08:24 +0530

    ListTile,circle Avatar Implement,reversedchartDays

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,90 +6,86 @@ import 'package:intl/intl.dart';
 // The Instance of this class is called inside the UserTransactions.dart file. The main motive of this seperate file is to make th code more manageable
 
 class TransactionList extends StatelessWidget {
-//  final ScrollController _controllerOne = ScrollController();
   final List<Transaction> userTransaction;
   TransactionList(this.userTransaction);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: userTransaction.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  "No Transactions Added yet!",
-                  style: Theme.of(context).textTheme.headline5,
-                  //textScaleFactor: 0.8,
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  height: 150,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.fitHeight,
+      child: SingleChildScrollView(
+        child: Column(
+          children: userTransaction.map((tx) {
+            return Card(
+              shadowColor: Colors.purple,
+              elevation: 4,
+              margin: EdgeInsets.symmetric(vertical: 7, horizontal: 4),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 24,
+                  child: FittedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text("\u{20B9}${tx.amount}"),
+                    ),
                   ),
                 ),
-              ],
-            )
-          : Scrollbar(
-              thickness: 4,
-              radius: Radius.circular(20),
-              //  controller: _controllerOne,
-              child: ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            border: Border.all(
-                              color: Theme.of(context).primaryColor,
-                              width: 2,
-                            ),
-                          ),
-                          child: Text(
-                            '\u{20B9}${userTransaction[index].amount.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              userTransaction[index].title,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('h:mm a | d MMM, y')
-                                  .format(userTransaction[index].date),
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
-                itemCount: userTransaction.length,
+                title: Text(
+                  tx.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(DateFormat('h:mm a | d MMM, y').format(tx.date)),
               ),
-            ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
+
+// Old card view chosen to Display the tiles, now I use the List Tile.
+/*return Card(
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      '\u{20B9}${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('h:mm a | d MMM, y').format(tx.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );*/
