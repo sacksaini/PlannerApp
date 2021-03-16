6c6deba5dc62d5c1e594660163a603494c7aca59
Sachin <sachinsaini854@gmail.com>
2021-03-16 19:23:43 +0530

    Responsiveness Improvement: MediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -84,29 +84,40 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Personal Expenses',
-          // style: TextStyle(
-          //   fontFamily: 'Quicksand',
-          //   fontSize: 17,
-          //   fontWeight: FontWeight.bold),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          ),
-        ],
+    final appbar = AppBar(
+      title: Text(
+        'Personal Expenses',
+        // style: TextStyle(
+        //   fontFamily: 'Quicksand',
+        //   fontSize: 17,
+        //   fontWeight: FontWeight.bold),
       ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: appbar,
       body: SingleChildScrollView(
         child: Column(
           //  mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_userTransaction, _deleteTransaction),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appbar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.4,
+                child: Chart(_recentTransactions)),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appbar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.6,
+                child: TransactionList(_userTransaction, _deleteTransaction)),
             // In this instance of a class, we call the other two widgets too i.e. NewTransactionand TransactionList.
             //Reason is that , in casewe want to change something particulary in List, then we can use onlt TransactionList and if only add a transaction, then call NewTransaction.
             //But instances of both classes are always called with this instance of UserTransactions.
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,61 +11,57 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.userTransaction, this.deleteTx);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 580,
-      child: userTransaction.isEmpty
-          ? Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "No Transactions Added!",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                    height: 100,
-                    child: Image.asset("assets/images/waiting.png"))
-              ],
-            )
-          : SingleChildScrollView(
-              child: Column(
-                children: userTransaction.map((tx) {
-                  return Card(
-                    shadowColor: Colors.purple,
-                    elevation: 4,
-                    margin: EdgeInsets.symmetric(vertical: 7, horizontal: 4),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 24,
-                        child: FittedBox(
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Text("\u{20B9}${tx.amount}"),
-                          ),
+    return userTransaction.isEmpty
+        ? Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "No Transactions Added!",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                  height: 100, child: Image.asset("assets/images/waiting.png"))
+            ],
+          )
+        : SingleChildScrollView(
+            child: Column(
+              children: userTransaction.map((tx) {
+                return Card(
+                  shadowColor: Colors.purple,
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(vertical: 7, horizontal: 4),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 24,
+                      child: FittedBox(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text("\u{20B9}${tx.amount}"),
                         ),
                       ),
-                      title: Text(
-                        tx.title,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle:
-                          Text(DateFormat('h:mm a | d MMM, y').format(tx.date)),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => deleteTx(tx.id),
-                        color: Theme.of(context).errorColor,
-                      ),
                     ),
-                  );
-                }).toList(),
-              ),
+                    title: Text(
+                      tx.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle:
+                        Text(DateFormat('h:mm a | d MMM, y').format(tx.date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => deleteTx(tx.id),
+                      color: Theme.of(context).errorColor,
+                    ),
+                  ),
+                );
+              }).toList(),
             ),
-    );
+          );
   }
 }
 
